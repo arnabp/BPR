37e315d9292871ecfa9982e70d9df13333a9e0ee
arnabp <arnab.purkayastha@colorado.edu>
2017-12-29 13:42:02 -0700

    Added EU/NA database tables

--- leaderboard_creation.sql
+++ leaderboard_creation.sql
@@ -1,5 +1,21 @@
 Use BHP;
-CREATE TABLE leaderboard(
+CREATE TABLE leaderboardNA(
 	id BIGINT PRIMARY KEY,
 	username VARCHAR(20),
     elo DECIMAL(6,2) DEFAULT 2500.00);
+CREATE TABLE matchesNA(
+	number INT PRIMARY KEY,
+    id1 BIGINT,
+    id2 BIGINT,
+    username1 VARCHAR(20),
+    username2 VARCHAR(20));
+CREATE TABLE leaderboardEU(
+	id BIGINT PRIMARY KEY,
+	username VARCHAR(20),
+    elo DECIMAL(6,2) DEFAULT 2500.00);
+CREATE TABLE matchesEU(
+	number INT PRIMARY KEY,
+    id1 BIGINT,
+    id2 BIGINT,
+    username1 VARCHAR(20),
+    username2 VARCHAR(20));
