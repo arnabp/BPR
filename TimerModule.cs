d80fd1a2dba76c358f6644222d3e2273b9a85cdf
arnabp <arnab.purkayastha@colorado.edu>
2018-12-22 03:25:29 -0700

    Space for field value now

--- TimerModule.cs
+++ TimerModule.cs
@@ -424,7 +424,7 @@ public class TimerService
                     embed1.AddField(x =>
                     {
                         x.Name = $"{reader.GetString(0)}: {reader.GetInt16(1)}";
-                        x.Value = "";
+                        x.Value = " ";
                         x.IsInline = true;
                     });
                 }
@@ -433,7 +433,7 @@ public class TimerService
                     embed2.AddField(x =>
                     {
                         x.Name = $"{reader.GetString(0)}: {reader.GetInt16(1)}";
-                        x.Value = "";
+                        x.Value = " ";
                         x.IsInline = true;
                     });
                 }
