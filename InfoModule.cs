c8ad55e3b14cd53cc8a25b69b83fdacd20a32cf4
arnabp <arnab.purkayastha@colorado.edu>
2019-12-31 18:08:28 -0800

    Set null check with equals sign, brought back delete message

--- InfoModule.cs
+++ InfoModule.cs
@@ -559,12 +559,12 @@ namespace BPR
         [Summary("Joins the 1v1 queue")]
         public async Task JoinAsync()
         {
-            if (localContext is null)
+            if (localContext == null)
             {
                 localContext = Context;
             }
             var userInfo = localContext.User;
-            //await localContext.Message.DeleteAsync();
+            await localContext.Message.DeleteAsync();
             Console.WriteLine($"{userInfo.Username} is attempting to join 1v1 queue");
 
             // Get User's information from Role
